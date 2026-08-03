a23f0ff26f62b49256a759e97b6a4cf2422c7f88
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2026-08-03 21:45:30 +0200

    Fix winget version post-check (#142)

--- modules/WingetMaintainerModule/Public/Get-LatestVersionInWinget.ps1
+++ modules/WingetMaintainerModule/Public/Get-LatestVersionInWinget.ps1
@@ -3,23 +3,18 @@ function Get-LatestVersionInWinget {
         [Parameter(Mandatory = $true)] [string] $PackageId
     )
 
-    $packagePath = $PackageId -replace '\.', '/'
-    $firstChar = $PackageId[0].ToString().ToLower()
-
     Write-Host "Checking if $PackageId is already in winget (via GH) and find latest Version"
-    
-    $ghVersionSearchString = "manifests/$($firstChar)/$($packagePath)/"
-    $ghResponse = gh search code $ghVersionSearchString $PackageId".yaml" --match path --repo microsoft/winget-pkgs -L 1000 --json path | ConvertFrom-Json
-    $versions = $ghResponse.path | ForEach-Object { $_ -replace ".*$ghVersionSearchString", "" -replace "/.*", "" } | Sort-Object -Descending -Unique
-    $sortedVersions = $versions| Get-STNumericalSorted -Descending
-    $latestVersion = $sortedVersions | Select-Object -First 1
+
+    $publishedVersionInfo = Get-WingetPublishedVersionsFromGitHub -PackageIdentifier $PackageId
+    $latestVersion = $publishedVersionInfo.Versions |
+        Sort-Object { Get-WingetSortableVersionKey -Version $_ } -Descending |
+        Select-Object -First 1
 
     if ($latestVersion) {
         Write-Host "Latest Version of $PackageId in Winget: $latestVersion"
         return $latestVersion
-    } 
-    else {
-        Write-Host "No Version found for Package $PackageId"
-        exit 1
     }
+
+    Write-Host "No Version found for Package $PackageId"
+    return $null
 }
